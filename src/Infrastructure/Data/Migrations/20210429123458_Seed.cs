8b2bd3fdb5106aead4322b20df67272208433d30
Berdan Atılım Şen <berdan_-_00505@hotmail.com>
2021-05-03 12:25:28 +0300

    HomeViewModelService

---
+++ src/Infrastructure/Data/Migrations/20210429123458_Seed.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class Seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert dbo.Authors(FullName) values ('Ahmet')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert dbo.Authors(FullName) values ('Ahmet')");
+        }
+    }
+}
