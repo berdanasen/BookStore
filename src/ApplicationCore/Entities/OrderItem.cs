dcb5f6b74de8041ffb6d278e4c53846112bb0037
Berdan Atılım Şen <berdan_-_00505@hotmail.com>
2021-05-05 13:21:23 +0300

    Order Basket Migration and Cart, AddtoCart Design

--- src/ApplicationCore/Entities/OrderItem.cs
+++ src/ApplicationCore/Entities/OrderItem.cs
@@ -9,11 +9,10 @@ namespace ApplicationCore.Entities
     public class OrderItem : BaseEntity
     {
         public int ProductId { get; set; }
+        public int OrderId { get; set; }
         public string ProductName { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
         public string PictureUri { get; set; }
-
-
     }
 }
