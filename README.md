1520dae6fc4aacc0a73a31e6b0614bfd6ea5d490
Berdan Atılım Şen <berdan_-_00505@hotmail.com>
2021-05-07 09:42:24 +0300

    BasketItemCounts BasketServices

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+
+
 First set src/Infrastructure as default project in Package Manager Console 
 
 Add-migration InitialApp -OutputDir Data/Migrations -Context Infrastructure.Data.AppDbContext -StartupProject Web
@@ -7,3 +9,6 @@ update-database -context AppDbContext
 
 Add-migration InitialIdentity -OutputDir Identity/Migrations -Context Infrastructure.Identity.AppIdentityDbContext -StartupProject Web
 update-database -context Infrastructure.Identity.AppIdentityDbContext
+
+# Creating AntiForgeryToken
+https://docs.microsoft.com/en-us/aspnet/core/security/anti-request-forgery?view=aspnetcore-5.0#javascript
--- src/Web/Controllers/BasketController.cs
+++ src/Web/Controllers/BasketController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Web.Interfaces;
+using Web.ViewModels;
 
 namespace Web.Controllers
 {
@@ -10,9 +13,30 @@ namespace Web.Controllers
     // todo : appcore > basketservice > additemtobasket
     public class BasketController : Controller
     {
+        private readonly IBasketViewModelService _basketViewModelService;
+        private readonly IBasketService _basketService;
+
+        public BasketController(IBasketViewModelService basketViewModelService, IBasketService basketService)
+        {
+            _basketViewModelService = basketViewModelService;
+            _basketService = basketService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddToBasket(int productId, int quantity)
+        {
+            // application core projesinde BasketService : IBasketService
+            // AddItemToBasket(int basketId, int catalogItemId, int quantity)
+            var basketId = await _basketViewModelService.GetOrCreateBasketIdAsync();
+            await _basketService.AddItemToBasket(basketId, productId, quantity);
+
+            // sepetteki yeni öğe sayısını döndür.
+            return Json(await _basketViewModelService.GetBasketItemsCountViewModel(basketId));
         }
     }
 }
